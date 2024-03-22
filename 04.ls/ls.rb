c5bf52390e577ded21cc37496fd9c356e9bade64
hirokiej <123009866+hirokiej@users.noreply.github.com>
2024-03-22 16:48:44 +0900

    ls -aを作成しました

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,20 +1,30 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require 'optparse'
+
 # 表示する列の数を定義
 COLUMN_NUMBER = 3
 
+all_files = false
+
+opt = OptionParser.new
+opt.on('-a', '--all [ITEM]', 'show all items') do
+  all_files = true
+end
+opt.parse(ARGV)
+
 # 配列の文字数を均一にする
-def format_file_name
-  files = Dir.glob('*')
+def format_file_name(all_files)
+  files = all_files ? Dir.glob('*', File::FNM_DOTMATCH) : Dir.glob('*')
   max_length = files.max_by(&:length).size
   files.map { |file| file.ljust(max_length + 1) }
 end
 
 # 要素を display_number ずつに分けて縦表示対応した配列にする
-def change_array_number(display_number)
+def change_array_number(display_number, all_files)
   n = display_number
-  files = format_file_name
+  files = format_file_name(all_files)
   add_blank_element = n - (files.size % n)
   files += [''] * add_blank_element
   array_for_right_position = files.each_slice(files.size / n).to_a
@@ -22,6 +32,6 @@ def change_array_number(display_number)
 end
 
 # 引数の数値で列数を変更
-change_array_number(COLUMN_NUMBER).each do |row|
+change_array_number(COLUMN_NUMBER, all_files).each do |row|
   puts row.join(' ')
 end
