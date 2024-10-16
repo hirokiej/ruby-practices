29dbbf8dabf70426ceb2a2b36c194b9e04142243
hirokiej <123009866+hirokiej@users.noreply.github.com>
2024-10-17 01:24:41 +0900

    lsの形式で出力できるようにfilename.rbを作成しました

--- 07.ls_object/filename.rb
+++ 07.ls_object/filename.rb
@@ -1,12 +1,31 @@
 #!/usr/bin/env ruby
 
 class FileName
+  COLUMN_NUMBER = 3
+
   attr_reader :files
 
   def initialize
     @files = Dir.glob('*')
   end
+
+  def align_file_name
+    max_length = @files.max_by(&:length).size
+    @files.map { |file| file.ljust(max_length + 1) }
+  end
+
+  def short_format
+    aligned_files = align_file_name
+    aligned_files.each_slice(COLUMN_NUMBER)
+  end
+
+  def display
+    short_format.each do |line|
+      puts line.join(' ')
+    end
+  end
 end
 
-file_name = FileName.new
-puts file_name.files
+
+file_name =FileName.new
+file_name.display
