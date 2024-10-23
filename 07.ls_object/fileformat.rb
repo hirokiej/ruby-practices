8aab1757939ba2e1beaf1787e0468c6f93b7c37f
hirokiej <123009866+hirokiej@users.noreply.github.com>
2024-10-23 11:42:05 +0900

    ロングフォーマットの必要な最長可変数を取得しフォーマットを整えました

--- 07.ls_object/fileformat.rb
+++ 07.ls_object/fileformat.rb
@@ -6,30 +6,31 @@ require_relative 'filedetails'
 class FileFormat
   def initialize(files)
     @files = files
+    @file_details = @files.map{ |file| FileDetails.new(file) }
   end
 
   def format_files
     @files.each do |file|
-      formatter(file)
+      details = FileDetails.new(file)
+      formatter(details)
     end
   end
 
-  def formatter(file)
-    details = FileDetails.new(file)
-    puts "#{details.filetype}#{details.permission} #{details.hard_link} #{details.owner_name} #{details.group_name}  #{details.bite_size}  #{details.update_time} #{file}"
+  def formatter(details)
+    puts "#{details.filetype}#{details.permission} #{details.hard_link.rjust(max_length[:link])} #{details.owner_name.rjust(max_length[:owner])}  #{details.group_name.rjust(max_length[:group])} #{details.bite_size.rjust(max_length[:size])}  #{details.update_time} #{details.filename}"
   end
 
   def max_length
-    hard_links = @files.map(&:hard_link)
-    owners = @files.map(&:owenr_name)
-    groups = @files.map(&:group_name)
-    sizez = @files.map(&:bite_size)
+    hard_links = @file_details.map(&:hard_link)
+    owners = @file_details.map(&:owner_name)
+    groups = @file_details.map(&:group_name)
+    sizes = @file_details.map(&:bite_size)
 
     {
       link: hard_links.max_by(&:length).length,
       owner: owners.max_by(&:length).length,
       group: groups.max_by(&:length).length,
-      size: sizez.max_by(&:length).length
+      size: sizes.max_by(&:length).length
     }
   end
 end
