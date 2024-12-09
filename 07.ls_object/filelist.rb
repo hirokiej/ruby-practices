5cf4c6cef0a1180c43ceb94a0b443df8088e90b3
hirokiej <123009866+hirokiej@users.noreply.github.com>
2024-12-09 12:28:39 +0900

    インスタンス変数Optionsに明示的にオプションをハッシュとして持たせることで、可読性を向上させました

--- 07.ls_object/filelist.rb
+++ 07.ls_object/filelist.rb
@@ -6,8 +6,8 @@ class FileList
   attr_reader :files
 
   def initialize(options)
-    @options = options
-    @files = Dir.glob('*', @options.all_files? ? File::FNM_DOTMATCH : 0)
-    @files.reverse! if @options.reverse_files?
+    @options = { all_files: options.all_files?, reverse_files: options.reverse_files?}
+    @files = Dir.glob('*', @options[:all_files] ? File::FNM_DOTMATCH : 0)
+    @files.reverse! if @options[:reverse_files]
   end
 end
